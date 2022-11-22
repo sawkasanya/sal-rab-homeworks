9c7d088d8d80fa714ab7a92883651a404e304cfd
Sawka Sanya <sasha_filippow@mail.ru>
2022-11-22 20:27:38 +0300

    Task 4.1

--- task_4/solution.js
+++ task_4/solution.js
@@ -12,7 +12,7 @@ return products;
 }
 function renderProductsCards(json){
 clearProducts();
-parseProducts(json) = products;
+products = parseProducts(json);
 length = products.length;
 for (let i = 0; i < length; i +=1) {
 addProduct(products[i]);
